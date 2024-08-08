2755b043dcf527be87a00905a19f5075761f01bc
Saikaranam-70 <saikaranam46@gmail.com>
2024-08-09 00:34:38 +0530

    first commit

--- src/index.jsx
+++ src/index.jsx
@@ -18,4 +18,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 //reportWebVitals();
-reportWebVitals
+reportWebVitals()
